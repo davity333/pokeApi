9d0914115c148579a6d5f47512d676b013755558
david guzman <reyguz421@gmail.com>
2024-12-03 10:59:04 -0600

    creando carpetas

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,13 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), provideHttpClient()
   ]
 };
 
